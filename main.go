584c2bd5782b5900a93683accd9b6fc07656de88
NikPeg <peganov.nik@gmail.com>
2024-10-20 18:44:47 +0300

    zero div again

--- main.go
+++ main.go
@@ -149,6 +149,9 @@ type Swimming struct {
 // длина_бассейна * количество_пересечений / м_в_км / продолжительность_тренировки
 // Это переопределенный метод Calories() из Training.
 func (s Swimming) meanSpeed() float64 {
+    if s.Duration.Hours() == 0 {
+        return 0
+    }
 	return float64(s.LengthPool) * float64(s.CountPool) / MInKm / s.Duration.Hours()
 }
 
